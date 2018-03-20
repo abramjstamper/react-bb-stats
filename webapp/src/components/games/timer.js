f4384eda3f11a2700c75c8d134636d089f2c6ee8
Abram Stamper <abram.j.stamper@me.com>
2018-03-20 10:59:04 -0400

    timer working

--- webapp/src/components/games/timer.js
+++ webapp/src/components/games/timer.js
@@ -7,7 +7,7 @@ class Timer extends Component {
     super();
 
     this.timer = {};
-    this.timer = this.createTimer(480);
+    this.timer = this.createTimer(15);
 
     this.createTimer = this.createTimer.bind(this);
     this.getSecondsAsDigitalClock = this.getSecondsAsDigitalClock.bind(this);
@@ -64,6 +64,7 @@ class Timer extends Component {
       this.props.updateClock(this.game, { displayTime: this.timer.displayTime, runTimer: true });
       console.log("Timer Ticking")
       console.log(this.timer.displayTime);
+      this.forceUpdate();
       if (this.timer.secondsRemaining > 0) {
         this.timerTick();
       }
@@ -74,7 +75,7 @@ class Timer extends Component {
   }
 
   getSecondsAsDigitalClock(inputSeconds) {
-    var sec_num = parseInt(inputSeconds.toString(), 10); // don't forget the second param
+    var sec_num = parseInt(inputSeconds.toString(), 10);
     var hours = Math.floor(sec_num / 3600);
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
     var seconds = sec_num - (hours * 3600) - (minutes * 60);
@@ -90,39 +91,13 @@ class Timer extends Component {
       return hoursString + ':' + minutesString + ':' + secondsString;
   }
 
-  renderTimerSwitch = () => {
-    console.log(!this.props.games[this.gameId].clock.runTimer);
-    if (!this.props.games[this.gameId].clock.runTimer) {
-      return this.renderStartTimer();
-    } else {
-      return this.renderStartGame();
-    }
-  }
-
-  renderStartGame = () => {
-    return (
-      <div>
-        <span>{this.props.games[this.props.location.payload.id].clock.displayTime}</span>
-        <button className="button" onClick={() => this.startTimer()}>Start Game</button>
-      </div>
-    );
-  }
-
-  renderStartTimer = () => {
-    return (
-      <div>
-        <span>{this.props.games[this.props.location.payload.id].clock.displayTime}</span>
-        <button className="button" onClick={() => this.startTimer()}>Start Timer</button>
-      </div>
-    );
-  }
-
   render() {
     return (
       <div className="card">
         <div className="card-content">
           <div className="content has-text-centered">
-            {this.renderTimerSwitch()}
+            <button className="button" onClick={() => this.startTimer()}>Start Timer</button>
+            <span>{this.props.games[this.gameId].clock.displayTime}</span>
           </div>
         </div>
       </div>
@@ -130,5 +105,5 @@ class Timer extends Component {
   }
 }
 
-const mapStateToProps = state => state;
+const mapStateToProps = state => {return {games: state.games, location: state.location}};
 export default connect(mapStateToProps, { updateClock })(Timer);
