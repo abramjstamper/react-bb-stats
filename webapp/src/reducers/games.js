15b52cde35391e92f798575e3df079d7cebcace9
Abram Stamper <abram.j.stamper@me.com>
2018-04-13 17:08:44 -0400

    add additional scenarios

--- webapp/src/reducers/games.js
+++ webapp/src/reducers/games.js
@@ -31,27 +31,19 @@ function games(state = {}, action) {
         clone[action.game.info.id].temp.actionText = "Select Player for FG Make"
         switch (action.location) {
           case "left side outside shot":
-            clone[action.game.info.id].temp.lastEventAction = "3PT_FG";
-            // clone[action.game.info.id].temp.awayScore = 3 + (clone[action.game.info.id].temp.awayScore || 0);
-            break;
           case "right side outside shot":
             clone[action.game.info.id].temp.lastEventAction = "3PT_FG";
-            // clone[action.game.info.id].temp.homeScore = 3 + (clone[action.game.info.id].temp.homeScore || 0);
             break;
           case "left side layups":
           case "left side dunk":
           case "left side inside shot":
           case "left side paint":
           case "left side basket":
-            // clone[action.game.info.id].temp.awayScore = 2 + (clone[action.game.info.id].temp.awayScore || 0);
-            clone[action.game.info.id].temp.lastEventAction = "2PT_FG";
-            break;
           case "right side layups":
           case "right side dunk":
           case "ride side inside shot":
           case "right side paint":
           case "right side basket":
-            // clone[action.game.info.id].temp.homeScore = 2 + (clone[action.game.info.id].temp.homeScore || 0);
             clone[action.game.info.id].temp.lastEventAction = "2PT_FG";
             break;
         }
@@ -59,27 +51,19 @@ function games(state = {}, action) {
         clone[action.game.info.id].temp.actionText = "Select Player for FG Miss"
         switch (action.location) {
           case "left side outside shot":
-            clone[action.game.info.id].temp.lastEventAction = "MISSED_3PT_FG";
-            // clone[action.game.info.id].temp.awayScore = 3 + (clone[action.game.info.id].temp.awayScore || 0);
-            break;
           case "right side outside shot":
             clone[action.game.info.id].temp.lastEventAction = "MISSED_3PT_FG";
-            // clone[action.game.info.id].temp.homeScore = 3 + (clone[action.game.info.id].temp.homeScore || 0);
             break;
           case "left side layups":
           case "left side dunk":
           case "left side inside shot":
           case "left side paint":
           case "left side basket":
-            // clone[action.game.info.id].temp.awayScore = 2 + (clone[action.game.info.id].temp.awayScore || 0);
-            clone[action.game.info.id].temp.lastEventAction = "MISSED_2PT_FG";
-            break;
           case "right side layups":
           case "right side dunk":
           case "ride side inside shot":
           case "right side paint":
           case "right side basket":
-            // clone[action.game.info.id].temp.homeScore = 2 + (clone[action.game.info.id].temp.homeScore || 0);
             clone[action.game.info.id].temp.lastEventAction = "MISSED_2PT_FG";
             break;
         }
@@ -97,6 +81,15 @@ function games(state = {}, action) {
         case 'FOUL':
           clone[action.game.info.id].events[key] = { playerId: action.player.id, eventId: 9, time: action.game.clock.displayTime, gameId: action.game.info.id, period: action.game.clock.period }
           break;
+        case 'OFFENSIVE_FOUL':
+          clone[action.game.info.id].events[key] = { playerId: action.player.id, eventId: 10, time: action.game.clock.displayTime, gameId: action.game.info.id, period: action.game.clock.period }
+          break;
+        case 'TECHNICAL_FOUL':
+          clone[action.game.info.id].events[key] = { playerId: action.player.id, eventId: 11, time: action.game.clock.displayTime, gameId: action.game.info.id, period: action.game.clock.period }
+          break;
+        case 'BLOCK':
+          clone[action.game.info.id].events[key] = { playerId: action.player.id, eventId: 12, time: action.game.clock.displayTime, gameId: action.game.info.id, period: action.game.clock.period }
+          break;
         case "MISSED_2PT_FG":
           clone[action.game.info.id].events[key] = { playerId: action.player.id, eventId: 3, time: action.game.clock.displayTime, gameId: action.game.info.id, period: action.game.clock.period }
           break;
@@ -105,9 +98,11 @@ function games(state = {}, action) {
           break;
         case "2PT_FG":
           clone[action.game.info.id].events[key] = { playerId: action.player.id, eventId: 5, time: action.game.clock.displayTime, gameId: action.game.info.id, period: action.game.clock.period }
+          // clone[action.game.info.id].temp.homeScore = 2 + (clone[action.game.info.id].temp.homeScore || 0);
           break;
         case "3PT_FG":
           clone[action.game.info.id].events[key] = { playerId: action.player.id, eventId: 6, time: action.game.clock.displayTime, gameId: action.game.info.id, period: action.game.clock.period }
+          // clone[action.game.info.id].temp.homeScore = 3 + (clone[action.game.info.id].temp.homeScore || 0);
           break;
       }
       console.log(clone[action.game.info.id].events);
